4ed941022410c622a49a743c7dd3d441b53ddaca
Yura Kocyuba <yurakocyuba@yandex.ru>
2019-09-08 21:32:30 +0300

    added first unit test

---
+++ src/test/java/eosio/spectrum/websocket/api/SubscriberSessionStorageTest.java
@@ -0,0 +1,25 @@
+package eosio.spectrum.websocket.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubscriberSessionStorageTest {
+
+    @Test
+    public void getAccounts() {
+
+    }
+
+    @Test
+    public void saveSessionIdAccounts() {
+    }
+
+    @Test
+    public void removeSessionIdAccounts() {
+    }
+
+    @Test
+    public void getSessionId() {
+    }
+}
